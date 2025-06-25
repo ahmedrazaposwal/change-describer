1133e4d2fc349e823c46def1d037dc490f75136b
Ahmed Raza <ahmedrazaposwal@Ahmeds-MacBook-Air.local>
2025-06-25 14:51:15 +0200

    Restored original README and local files

---
+++ src/extension.ts
@@ -0,0 +1,71 @@
+import * as vscode from "vscode";
+import { exec } from "child_process";
+import axios from 'axios';
+import { resolve } from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand("arpdodo.describeChanges", async () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (!workspaceFolders) {
+			vscode.window.showErrorMessage("No workspace folder is open.");
+			return;
+		}
+		
+		const cwd = workspaceFolders[0].uri.fsPath;
+		exec('git diff --cached', { cwd }, async(err, stdout) => {
+
+			if (err || !stdout.trim()) {
+				vscode.window.showInformationMessage('No staged changes found. Please run `git add .`');
+				return;
+			}
+
+			const diff = stdout;
+
+			const apiKey = "api_REPLACE_xjkdhjshd";
+			if (!apiKey || apiKey.startsWith("api_REPLACE")) {
+				vscode.window.showErrorMessage("Set your api key");
+				return;
+			}
+
+			try {
+				const summary = await askOpenAI(diff, apiKey);
+				const doc = await vscode.workspace.openTextDocument({
+					content: `Change Summary\n\n${summary}`,
+					language: 'markdown'
+				});
+				vscode.window.showTextDocument(doc);
+			} catch (e: any) {
+				vscode.window.showErrorMessage(`Failed to generate summary: ${e.message}`);
+			}
+		});
+	});
+	context.subscriptions.push(disposable);
+}
+
+
+async function askOpenAI(diff:string, apiKey:string): Promise<string> {
+	const response = await axios.post(
+		'https://api.openai.com/v1/chat/completions',
+		{
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{
+					role: 'system',
+					content: 'You are a senior developer who summarizes Git diff.'
+				},
+				{
+					role: 'user',
+					content: `Explain these Git changes: what was changed, why it was changed, and how:\n\n${diff}`
+				}
+			]
+		},
+		{
+			headers: {
+				Authorization: `Brearer ${apiKey}`
+			}
+		}
+	);
+
+	return response.data.choices[0].message.content.trim();
+}
+export function deactivate() {}
